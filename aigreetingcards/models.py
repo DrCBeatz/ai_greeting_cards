3ae269231463401eebdb13c63d694122b41996f0
DrCBeatz <brynecarruthers@gmail.com>
2024-07-07 17:25:52 -0400

    add image_file thumbnail

--- aigreetingcards/models.py
+++ aigreetingcards/models.py
@@ -5,14 +5,18 @@ from django.core.files import File
 from urllib import request
 from django.contrib.auth.models import User
 from django.conf import settings
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill, Transpose
 
 import os
 
 class Image(models.Model):
     image_file = models.ImageField(upload_to='images', max_length=500)
     image_url = models.URLField(max_length=500)
+    thumbnail = ImageSpecField(source='image_file', id='aigreetingcards:image_file:image_file_thumbnail', processors=[Transpose(),ResizeToFill(650, 650)], format='JPEG', options={'quality':100})
     title = models.CharField(max_length=500, null=True, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    test = models.BooleanField(default=False)
 
     def get_remote_image(self):
         if self.image_url and not self.image_file:
