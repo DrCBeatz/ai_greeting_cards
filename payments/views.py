4b94e486c1ffc74d9537766b25d8bdb68cfe878d
DrCBeatz <brynecarruthers@gmail.com>
2024-07-25 23:39:09 -0400

    add credits paid for in StripeWebhookView

--- payments/views.py
+++ payments/views.py
@@ -8,16 +8,21 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.shortcuts import redirect
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
 
 PRICE_PER_CREDIT = 0.50 # $0.50 for 10 credits
+
+User = get_user_model()
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 class CreateStripeCheckoutSessionView(View):
 
     def post(self, request, *args, **kwargs):
-        credits = int(request.POST.get("credits", 10)) # Default to 10 credits
-        price_in_cents = int(PRICE_PER_CREDIT * credits * 10)
+        credits = int(request.POST.get("credits", 10))  # Default to 10 credits
+        price_in_cents = int(PRICE_PER_CREDIT * credits * 10)  # Amount in cents
+        user_id = request.user.id  # Get the logged-in user's ID
 
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=["card"],
@@ -34,7 +39,7 @@ class CreateStripeCheckoutSessionView(View):
                     "quantity": "1",
                 }
             ],
-            metadata={"product_id": "1"},
+            metadata={"user_id": user_id},  # Include the user ID in the metadata
             mode="payment",
             success_url=settings.PAYMENT_SUCCESS_URL,
             cancel_url=settings.PAYMENT_CANCEL_URL,
@@ -70,6 +75,18 @@ class StripeWebhookView(View):
             return HttpResponse(status=400)
         
         if event["type"] == "checkout.session.completed":
-            print("Payment successful")
+            session = event['data']['object']
+            user_id = session["metadata"]["user_id"]
+            amount_total = session["amount_total"]
+            user = User.objects.get(id=user_id)
+
+            # Calculate the number of credits
+            credits_to_add = int(amount_total / (PRICE_PER_CREDIT * 10))  # amount_total is in cents
+
+            # Update the user's credits
+            user.credits += credits_to_add
+            user.save()
+            
+            print(f"Payment successful. Added {credits_to_add} credits to user ID {user_id}")
         
         return HttpResponse(status=200)
