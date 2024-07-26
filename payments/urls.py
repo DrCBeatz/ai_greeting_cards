401d2644d290f4cc788c8c826bcf60b2eecfc297
DrCBeatz <brynecarruthers@gmail.com>
2024-07-25 20:50:03 -0400

    add StripeWebhookView to Payments app

--- payments/urls.py
+++ payments/urls.py
@@ -2,7 +2,13 @@
 
 from django.urls import path
 
-from .views import BuyCreditsView, CreateStripeCheckoutSessionView, SuccessView, CancelView
+from .views import (
+    BuyCreditsView, 
+    CreateStripeCheckoutSessionView, 
+    SuccessView, 
+    CancelView, 
+    StripeWebhookView
+    )
 
 urlpatterns = [
     path("buy-credits/", BuyCreditsView.as_view(), name="buy-credits"),
@@ -13,4 +19,5 @@ urlpatterns = [
          ),
     path("success/", SuccessView.as_view(), name="success"),
     path("cancel/", CancelView.as_view(), name="cancel"),
+    path("webhooks/stripe/", StripeWebhookView.as_view(), name="stripe-webhook"),
 ]
--- payments/views.py
+++ payments/views.py
@@ -2,9 +2,12 @@
 
 import stripe
 from django.conf import settings
-from django.shortcuts import redirect
 from django.views import View
 from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.shortcuts import redirect
+from django.http import HttpResponse
 
 PRICE_PER_CREDIT = 0.50 # $0.50 for 10 credits
 
@@ -47,3 +50,26 @@ class CancelView(TemplateView):
 
 class BuyCreditsView(TemplateView):
     template_name = "payments/buy_credits.html"
+
+@method_decorator(csrf_exempt, name="dispatch")
+class StripeWebhookView(View):
+
+    def post(self, request, format=None):
+        payload = request.body
+        endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
+        sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
+        event = None
+
+        try:
+            event = stripe.Webhook.construct_event(payload, sig_header, endpoint_secret)
+        except ValueError as e:
+            # Invalid payload
+            return HttpResponse(status=400)
+        except stripe.error.SignatureVerificationError as e:
+            # Invalid signature
+            return HttpResponse(status=400)
+        
+        if event["type"] == "checkout.session.completed":
+            print("Payment successful")
+        
+        return HttpResponse(status=200)
