db782fe41809ca296b65b7eb724ba8fdcc69fd7d
DrCBeatz <brynecarruthers@gmail.com>
2024-08-21 22:08:13 -0400

    add secrets to github actions

--- core/settings.py
+++ core/settings.py
@@ -152,7 +152,6 @@ if not S3_BUCKET_ENABLED:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 else:
-    # Optional: Custom domain for serving media files
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
