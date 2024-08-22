97efaf79e4e1cce964bda14584ba194ca6fe7b24
DrCBeatz <brynecarruthers@gmail.com>
2024-08-21 22:19:31 -0400

    change S3 path in settings.py

--- core/settings.py
+++ core/settings.py
@@ -153,7 +153,7 @@ if not S3_BUCKET_ENABLED:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 else:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+    MEDIA_URL = f'http://{AWS_S3_CUSTOM_DOMAIN}/media/'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Default primary key field type
