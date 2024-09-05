99df223d5e50a02a375ca61d5cf37dad01bf8415
DrCBeatz <brynecarruthers@gmail.com>
2024-09-04 22:15:51 -0400

    set admin path in core/urls.py with DJANGO_ADMIN_URL env variable

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
     path('', include('aigreetingcards.urls')),
     path("accounts/", include("accounts.urls")),
     path("payments/", include("payments.urls")),
