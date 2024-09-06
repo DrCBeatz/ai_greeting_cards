cae17587a0396fa969f0ad60e3a5875119abe30e
DrCBeatz <brynecarruthers@gmail.com>
2024-09-05 22:03:31 -0400

    add quality selection field to home image prompt form & change credit cost for standard vs hd

--- aigreetingcards/tasks.py
+++ aigreetingcards/tasks.py
@@ -10,7 +10,7 @@ import redis
 redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
 
 @shared_task
-def generate_image_task(prompt, size, user_id):
+def generate_image_task(prompt, size, quality, user_id):
     task_id = generate_image_task.request.id
     redis_client.set(f"task_status:{task_id}", "processig")
 
@@ -22,7 +22,7 @@ def generate_image_task(prompt, size, user_id):
             model="dall-e-3",
             prompt=prompt,
             size=size,
-            quality="standard",
+            quality=quality,
             n=1,
         )
 
@@ -31,8 +31,12 @@ def generate_image_task(prompt, size, user_id):
         new_image.get_remote_image()
         new_image.save()
 
-        # Deduct 10 credits from the user after successful image generation
-        user.credits -= 10
+        # Determine how many credits to deduct based on the quality
+        if quality == 'hd':
+            user.credits -= 20
+        else:
+            user.credits -= 10
+
         user.save()
         
         redis_client.set(f"task_status:{task_id}", "completed")
--- aigreetingcards/views.py
+++ aigreetingcards/views.py
@@ -36,15 +36,25 @@ def check_task_status(request, task_id):
 def home(request):
     prompt = ''
     if request.method == 'POST':
-        if request.user.credits >= 10:
-            prompt = request.POST.get('prompt')
-            size = request.POST.get('size')
-            task = generate_image_task.delay(prompt, size, request.user.id)
+        prompt = request.POST.get('prompt')
+        size = request.POST.get('size')
+        quality = request.POST.get('quality', 'standard')  # Default to 'standard' if not provided
+
+        # Determine the required credits based on the quality
+        if quality == 'hd':
+            required_credits = 20
+        else:
+            required_credits = 10
+
+        # Check if the user has enough credits
+        if request.user.credits >= required_credits:
+            # Start the asynchronous task without deducting credits here
+            task = generate_image_task.delay(prompt, size, quality, request.user.id)
             return HttpResponseRedirect(reverse('image_list') + f"?task_id={task.id}")
         else:
             messages.error(request, 'Insufficient credits to generate an image.')
             return redirect('home')
-            
+
     return render(request, 'home.html', {'prompt': prompt})
 
 class ImageListView(ListView):
