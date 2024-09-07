60faef6944c1082110bf35be0be62ae675fcb108
DrCBeatz <brynecarruthers@gmail.com>
2024-09-06 21:02:55 -0400

    add prevent_destroy lifecycle hook to hosted zone in route53.tf

--- route53.tf
+++ route53.tf
@@ -1,13 +1,18 @@
 # route53.tf
 
+# Create the Route 53 hosted zone
 resource "aws_route53_zone" "main" {
-    name = var.domain_name
+  name = var.domain_name
+
+  lifecycle {
+    prevent_destroy = true  # Prevent Terraform from deleting the hosted zone
+  }
 }
 
 # Create an A record that points to the ALB
 resource "aws_route53_record" "web_app" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = var.domain_name  # Use the root domain or a subdomain if needed (e.g., "www")
+  name    = var.domain_name  # Root domain or subdomain (e.g., "www")
   type    = "A"
 
   alias {
@@ -17,6 +22,7 @@ resource "aws_route53_record" "web_app" {
   }
 }
 
+# Create a CNAME record for the www subdomain
 resource "aws_route53_record" "www_web_app" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www.${var.domain_name}"
